b51ea3b81495c8c6541c21835e64b13c3f81f0be
Benjamin Bellamy <benjamin@bellamy.ovh>
2018-10-21 21:29:23 +0000

    Added hdmi-cec dependency

--- app.js
+++ app.js
@@ -13,4 +13,18 @@ roon.init_services({});
 
 roon.start_discovery();
 
+var cecRemote = require('hdmi-cec').Remote;
+ 
+// Create a new Remote helper (called without any arguments, it will create a cec-client process itself, with the default client name)
+var remote = new cecRemote();
+
+// When any button is pressed on the remote, we receive the event:
+remote.on('keypress', function(evt) {
+    console.log('user pressed the key "'+ evt.key + '" with code "' + evt.keyCode + '"');
+});
+ 
+// Alternatively, we only wait for the user to press the "select" key
+remote.on('keypress.select', function() {
+    console.log('user pressed the select key!');
+});
 
