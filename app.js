12a60eb808a83ee0fa0ab2bda1e3134ff8abbf7c
Benjamin Bellamy <benjamin@bellamy.ovh>
2018-10-22 22:34:09 +0000

    Changed CEC name +some stuff

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+/*
+ * Roon CEC Controller Extension should allow you to control Roon Core
+ * from your audio amplifier remote control, having your amplifier connected to a Roon Bridge via HDMI using CEC protocol.
+ */
+
+// Javascript is messy enough, let's use strict mode:
 "use strict";
 
-var cecRemote        = require('hdmi-cec').Remote, 
-    RoonApi          = require("node-roon-api"),
-    RoonApiSettings  = require('node-roon-api-settings'),
-    RoonApiStatus    = require('node-roon-api-status'),
-    RoonApiTransport = require('node-roon-api-transport');
+// We need a bunch of APIs:
+var cecMonitor           = require('hdmi-cec').CecMonitor,
+    cecRemote            = require('hdmi-cec').Remote, 
+    RoonApi              = require("node-roon-api"),
+    RoonApiSettings      = require('node-roon-api-settings'),
+    RoonApiStatus        = require('node-roon-api-status'),
+    RoonApiTransport     = require('node-roon-api-transport'),
+    RoonApiSourceControl = require('node-roon-api-source-control');
 
-var core;
+// The Roon Core:
+var core = undefined;
 
+// We create a new Remote helper, but first we create a monitor in order to be able to rename the CEC device name:
+var monitor = new cecMonitor('roon', 4, false);
+var remote = new cecRemote(monitor);
+
+// We create the RoonApi and try to connect to Roon Core
 var roon = new RoonApi({
     extension_id:        'ovh.bellamy.roon-cec-controller',
     display_name:        "Roon CEC Controller",
@@ -18,15 +33,19 @@ var roon = new RoonApi({
 
     core_paired: function(core_) {
         core = core_;
+        monitor.send('tx 45:47:72:6f:6f:6e:20:72:65:61:64:79');
     },
     core_unpaired: function(core_) {
 	core = undefined;
+	monitor.send('tx 45:47:72:6f:6f:6e:20:65:72:72:6f:72');
     }
 });
 
-
+// The Extension Settings:
 var mysettings = roon.load_config("settings") || {
+    // We need to know the roon zone where we will  be playing:
     zone:             null,
+    // this is just to know how many seconds fast forward button will fast forward:
     seekamount:       5
 };
 
@@ -59,6 +78,7 @@ function makelayout(settings) {
     return l;
 }
 
+// This was copied/pasted from an example, I have no idea why it's here:
 var svc_settings = new RoonApiSettings(roon, {
     get_settings: function(cb) {
         cb(makelayout(mysettings));
@@ -76,17 +96,16 @@ var svc_settings = new RoonApiSettings(roon, {
 });
 
 var svc_status = new RoonApiStatus(roon);
+var svc_source_control = new RoonApiSourceControl(roon);
 
+// We need Transport and we provide settings, status and source control:
 roon.init_services({
     required_services:   [ RoonApiTransport ],
-    provided_services:   [ svc_settings, svc_status ],
+    provided_services:   [ svc_settings, svc_status, svc_source_control ],
 });
 
 
-// Create a new Remote helper (called without any arguments, it will create a cec-client process itself, with the default client name)
-var remote = new cecRemote();
-
-//F5:47:72:6f:6f:6e
+// these are the CEC codes for the remote control buttons:
 //{"select", 0},
 //{"stop", 69},
 //{"pause", 70},
@@ -100,9 +119,10 @@ var remote = new cecRemote();
 //{"rewind", 72},
 //{"exit", 13},
 
-// When any button is pressed on the remote, we receive the event:
+
+// When any button is pressed on the remote, we receive the event and send it to roon core:
 remote.on('keypress', function(evt) {
-    console.log('{"'+ evt.key + '", ' + evt.keyCode + '}');
+    //console.log('{"'+ evt.key + '", ' + evt.keyCode + '}');
     if (!core) return;
     if     (evt.keyCode==0) core.services.RoonApiTransport.control(mysettings.zone, 'playpause');
     else if(evt.keyCode==68) core.services.RoonApiTransport.control(mysettings.zone, 'play');
@@ -110,8 +130,8 @@ remote.on('keypress', function(evt) {
     else if(evt.keyCode==69) core.services.RoonApiTransport.control(mysettings.zone, 'stop');
     else if(evt.keyCode==76 || evt.keyCode==3) core.services.RoonApiTransport.control(mysettings.zone, 'previous');
     else if(evt.keyCode==75 || evt.keyCode==4) core.services.RoonApiTransport.control(mysettings.zone, 'next');
-    else if(evt.keyCode==72) core.services.RoonApiTransport.seek(mysettings.zone, 'relative', -mysettings.seekamount);
-    else if(evt.keyCode==71) core.services.RoonApiTransport.seek(mysettings.zone, 'relative', mysettings.seekamount);
+    else if(evt.keyCode==72 || evt.keyCode==1) core.services.RoonApiTransport.seek(mysettings.zone, 'relative', -mysettings.seekamount);
+    else if(evt.keyCode==71 || evt.keyCode==2) core.services.RoonApiTransport.seek(mysettings.zone, 'relative', mysettings.seekamount);
 });
  
 roon.start_discovery();
